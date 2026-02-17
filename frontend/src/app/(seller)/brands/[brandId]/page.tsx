19f749ef01fbb3fc787f76dd4ad810bd0844ac6e
Idoma Ngbede <idomakevin@gmail.com>
2026-02-18 00:43:40 +0100

    fix yaml errors

--- frontend/src/app/(seller)/brands/[brandId]/page.tsx
+++ frontend/src/app/(seller)/brands/[brandId]/page.tsx
@@ -1,11 +1,7 @@
 import BrandProfile from "@/components/BrandProfile";
 import { brands } from "@/data/brands";
 
-export default async function page({
-  params,
-}: {
-  params: { brandId: string };
-}) {
+export default async function page({ params }: any) {
   const { brandId } = await params;
   const brand = brands.find((brand) => brand.id === +brandId);
   return (
