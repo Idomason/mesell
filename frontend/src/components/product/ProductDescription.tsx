60c9eb09f7ebe97e44914a4cbd7daae3877c6d80
Idoma Ngbede <idomakevin@gmail.com>
2026-02-19 22:58:40 +0100

    fix UX issue in ProductOverview component

--- frontend/src/components/product/ProductDescription.tsx
+++ frontend/src/components/product/ProductDescription.tsx
@@ -108,7 +108,7 @@ export default function ProductDescription({
               Buy now
             </button>
             <button className="inline-block rounded-md shadow p-3 bg-gray-100 hover:bg-accent-500 transition-all duration-300 ease-in-out border border-gray-300">
-              <HiOutlineHeart className="md:size-5" />
+              <HiOutlineHeart />
             </button>
           </div>
         </div>
