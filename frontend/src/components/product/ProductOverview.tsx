5ab0b91f6b957fffef84b3ac733397527a3bbd83
Idoma Ngbede <idomakevin@gmail.com>
2026-02-19 21:43:23 +0100

    fix UX issue in ProductOverview component

--- frontend/src/components/product/ProductOverview.tsx
+++ frontend/src/components/product/ProductOverview.tsx
@@ -14,25 +14,23 @@ export default function ProductOverview() {
 
   return (
     <div className="w-full max-w-6xl mx-auto pb-4">
-      <div className="grid md:grid-cols-[1fr_400px] justify-between bg-white">
+      <div className="grid grid-cols-1 md:grid-cols-[1fr_400px] justify-between bg-white">
         <div className="py-3">
           <hr className="h-0.5 bg-black/25" />
-          <div className="flex items-center">
-            {["description", "reviews", "brand", "history", "usage guide"].map(
-              (item) => (
-                <button
-                  key={item}
-                  onClick={() => handleShowDetail(item)}
-                  className={`py-1.5 -mb-0.5 px-5 capitalize font-medium hover:text-primary-500 transition-all duration-300 ease-in-out ${
-                    showDetail === item
-                      ? "border-b-2 border-primary-500 text-primary-500"
-                      : ""
-                  }`}
-                >
-                  {item}
-                </button>
-              ),
-            )}
+          <div className="flex items-center overflow-x-scroll">
+            {["description", "reviews", "history", "guide"].map((item) => (
+              <button
+                key={item}
+                onClick={() => handleShowDetail(item)}
+                className={`py-1.5 -mb-0.5 px-5 capitalize font-medium hover:text-primary-500 transition-all duration-300 ease-in-out ${
+                  showDetail === item
+                    ? "border-b-2 border-primary-500 text-primary-500"
+                    : ""
+                }`}
+              >
+                {item}
+              </button>
+            ))}
           </div>
           <hr className="h-0.5 bg-black/25" />
           <div className="px-2 md:px-8 py-10 bg-white">
